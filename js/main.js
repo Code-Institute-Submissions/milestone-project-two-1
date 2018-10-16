3660a93a2952f6e4681b5a028238feebc27b4d91
Alee Sang <aleesang20@gmail.com>
2018-10-16 01:19:57 +0000

    added functions to main.js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var rounds;  // what round player is on
 var simonTurn;  // boolean true or false - if simon's turn or player's turn 
 var strict = false; // if button has been pressed - starts at false 
 var sound = true;
-var on = false; // if power has been switched on - starts at false 
+var powerOn = false; // if power has been switched on - starts at false 
 var win; // player has won 
 
  // pass in css selector with ID tag 
@@ -19,3 +19,38 @@ var startButton = document.querySelector("#start");
 var strictButton = document.querySelector("#strict");
 
 
+strictButton.addEventListener('click', (event) => {
+    if (strictButton.checked == true) {
+        strict = true;
+    } else {
+        strict = false;
+    }
+});
+
+onButton.addEventListener('click', (event) => {
+    if (onButton.checked == true) {
+        on = true;
+        counterOn.innerHTML = "-";
+    } else {
+        on = false;
+        counterOn.innerHTML = "";
+        clearColor();
+        clearInterval(interval);
+    }
+});
+
+startButton.click(function() {
+  playerSequence();
+});
+
+
+
+
+  var interval = setInterval(function() {
+    $("#" + arr[i]).fadeTo("slow", 0).fadeTo("slow", 1);
+    $("#sound-" + arr[i])[0].play();
+    i++;
+    if (i >= arr.length) {
+      clearInterval(interval);
+    }
+  }, 1500);
