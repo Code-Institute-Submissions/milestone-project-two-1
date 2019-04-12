cd0c38bfef5a3346a78094470b39ac0e98b9ecb2
Alee Sang <aleesang20@gmail.com>
2019-04-12 17:29:43 +0000

    added animations to win function

--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
 /* global $ */
-$(document).ready(function () {
-
-
+$(function() {
 //Console Game Pieces
 var gameSequence = [], // order of buttons clicked by game
-    playerSequence = [], // order of buttons clicked by player
-    simonTurn = 0,
-    flashColor = 300, // number of times button flashes
+    playerSequence = [], // buttons clicked by player
+    simonTurn = 0, // start round at zero
     rounds = 0, // displays counter
     onButton = false, // starts game
     playerButton = false,
     strictButton = false,
+    flashColor = 200, // duration of buttons flashing colour
     colorButtons = ["green", "red", "yellow", "blue"];
 
 
@@ -83,38 +81,40 @@ playerButton = false;
 
 // Sound Effects - Audio files found here: https://learn.freecodecamp.org/coding-interview-prep/take-home-projects/build-a-simon-game & freesound.org
 var sounds = {
-    green: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3"),
-    red: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3"),
-    yellow: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"),
-    blue: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"),
-    win: new Audio("https://freesound.org/data/previews/109/109662_945474-lq.mp3"),
+    green: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"),
+    red: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"),
+    yellow: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3"),
+    blue: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3"),
+    win: new Audio("https://freesound.org/data/previews/428/428156_8014960-lq.mp3"),
     wrong: new Audio("https://freesound.org/data/previews/415/415764_6090639-lq.mp3"),
  };
 
 // Create Game Sequence
+    //-----Assigned the Math.random() function for this sequence. Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function press() {
-    gameSequence.push(colorButtons[Math.floor(Math.random() * colorButtons.length)]);
-    return setTimeout(counting, 300);
+    gameSequence.push(colorButtons[Math.floor(Math.random() * 4)]);
+    play();
+    return setTimeout(counting, 500);
 
 }
 
 // Game Sequence Activates
-function steps(i) {
-    $("." + gameSequence[i]).addClass("light");
-    sounds[gameSequence[i]].play();
+function steps(activate) {
+    $("." + gameSequence[activate]).addClass("light");
+    sounds[gameSequence[activate]].play();
 
     setTimeout(function() {
-        $("." + gameSequence[i]).removeClass("light");
+        $("." + gameSequence[activate]).removeClass("light");
     }, flashColor);
 }
 
 // Displays Rounds in Counter
 function counting() {
     rounds++;
-    $(".counter-text").text(rounds < 10 ? "0" + rounds : rounds);
+    $(".counter-text").text(rounds < 10 ? "0" + rounds : rounds); // If number less than 10, display eg: 03
 }
 
-function countingError() {
+function countingError() { // if user presses incorrectly, display ??
     rounds = 0;
     $(".counter-text").text("??");
 }
@@ -126,21 +126,22 @@ function play() {
     simonTurn = gameSequence.length;
     function loop() {
         if (i < simonTurn) {
-            playerFalse()
+            playerFalse();
             steps(i);
             i++;
             setTimeout(loop, interval);
         } else {
-            console.log(i)
-            playerTrue()
+            console.log(i);
+            playerTrue();
         }
     }
     setTimeout(loop, interval);
 }
 
 // Player Sequence
-function playerTurn(choice) {
-    var playerColor = $(choice).attr("id");
+//----- reference https://codepen.io/zentech/pen/XaYygR?editors=0010 line 63 - 80 in JS file
+function playerTurn(go) {
+    var playerColor = $(go).attr("id");
     playerSequence.push(playerColor);
     sounds[playerColor].play();
     $("." + playerColor).addClass("light");
@@ -161,7 +162,7 @@ function startMove() {
 // Player Makes Wrong Move
 function wrongMove() {
     playerSequence = [];
-    console.log("no");
+    console.log("??");
     sounds.wrong.play();
 }
 
@@ -169,25 +170,26 @@ function wrongMove() {
 // Player Wins
 function win() {
     setTimeout(function() {
-        win.play();
+        sounds.win.play();
         rounds = 0;
-        $(".counter-text").text("YAY!");
-        $(".counter-text").effect("bounce", 1);
-        $(".square-buttons").effect("bounce", 1);
-    }, 1000);
+        $(".counter-text").text("YAY!").animate({opacity: "0.2"}).animate({opacity: "1"}).animate({opacity: "0.2"}).animate({opacity: "1"}).animate({opacity: "0.2"}).animate({opacity: "1"});
+        $(".square-buttons").animate({opacity: "0.2"}).animate({opacity: "1"}).animate({opacity: "0.2"}).animate({opacity: "1"}).animate({opacity: "0.2"}).animate({opacity: "1"});
+    }, 1000); //Animation referenced from: https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_eff_ani_opacity
+    
 
     return setTimeout(function() {
         playerSequence = [];
         gameSequence = [];
         press();
         play();
-    }, 2000);
+    }, 5000); // Time to enjoy the win!
+    
 
 }
 
 // Validate Game Sequence against Player Sequence
 function validate() {
-    var playerLength = playerSequence.length - 1;
+    var playerLength = playerSequence.length -1;
     if (playerSequence[playerLength] !== gameSequence[playerLength]) {
         if (strictButton) { //strict button on
             playerFalse();
@@ -200,21 +202,22 @@ function validate() {
             $(".counter-text").text("??");
             setTimeout(function() {
                 $(".counter-text").text(rounds < 10 ? "0" + rounds : rounds);
-            }, 1200);
+            }, 1000);
             setTimeout(wrongMove, 500);
             setTimeout(play, 1000);
         }
     } else {
-        console.log("YAY!")
+        console.log("YAY!");
         if (playerSequence.join("") === gameSequence.join("")) {
             simonTurn = gameSequence.length;
 
-            if (simonTurn === 20) {
+            if (simonTurn === 3) {
                 return win();
+                
             } else {
                 playerSequence = [];
                 press();
-                console.log(gameSequence)
+                console.log(gameSequence);
                 return play();
             }
         }
